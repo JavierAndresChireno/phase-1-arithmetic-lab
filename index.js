537fb83b3d91d9ac3752da8a931d7a0bed18420f
Javier Chireno <javierchireno04@gmail.com>
2021-07-20 16:40:37 -0400

    commit tree 85d9a6f65cdb259e2a5440c7d55270d15fa415a7

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 8;
+const num4 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random(1) *10);  
+
+const mod = num3 / num4 ;
+
+const max = Math.max(2,3,20,12,16)
